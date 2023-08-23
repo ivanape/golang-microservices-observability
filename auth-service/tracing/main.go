b5f951e9c811da668adf2bcaf2f2839e874bba9b
VahagnMian <vahagn.mian@gmail.com>
2023-08-23 21:16:40 +0400

    Remove unnecessary prometheus configuration

--- auth-service/tracing/main.go
+++ auth-service/tracing/main.go
@@ -7,38 +7,6 @@ import (
 	"io"
 )
 
-//func InitTracer(serviceName string) {
-//	JAEGER_ENDPOINT := "http://otel:14268/api/traces"
-//	// Create Jaeger exporter
-//	exporter, err := jaeger.New(
-//
-//		jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(JAEGER_ENDPOINT)),
-//	)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	// Resource can be modelled after the service
-//	resource1 := resource.NewWithAttributes(
-//		semconv.SchemaURL,
-//		semconv.ServiceNameKey.String(serviceName),
-//		semconv.ServiceVersionKey.String("v0.0.1"),
-//	)
-//
-//	// Tracer Provider
-//	tp := sdktrace.NewTracerProvider(
-//		sdktrace.WithBatcher(exporter),
-//		sdktrace.WithResource(resource1),
-//		// Configure the sampler to always sample.
-//		sdktrace.WithSampler(sdktrace.AlwaysSample()),
-//	)
-//
-//	otel.SetTracerProvider(tp)
-//
-//	// Set the propagator to tracecontext (the default is baggagetracecontext)
-//	otel.SetTextMapPropagator(propagation.TraceContext{})
-//}
-
 func InitTracer(serviceName string) io.Closer {
 	// Jaeger agent endpoint
 	agentEndpoint := "jaeger:6831"
