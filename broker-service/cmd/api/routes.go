bfc46d243b9d990983ad324e4a6dae41725f9f49
Iván Álvarez <ivanape@inditex.com>
2025-02-17 17:01:04 +0100

    [feat] - OTEL metrics scrape
    Signed-off-by: Iván Álvarez <ivanape@inditex.com>

--- broker-service/cmd/api/routes.go
+++ broker-service/cmd/api/routes.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/cors"
 	"github.com/riandyrn/otelchi"
 	otelchimetric "github.com/riandyrn/otelchi/metric"
 )
@@ -14,25 +13,15 @@ import (
 func (app *Config) routes(metricConfig otelchimetric.BaseConfig) http.Handler {
 	r := chi.NewRouter()
 
-	// specify who is allowed to connect
-	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://*", "http://*"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: true,
-		MaxAge:           300,
-	}),
+	r.Use(
 		otelchi.Middleware(obs.DefaultServiceTags["service"], otelchi.WithChiRoutes(r)),
 		otelchimetric.NewRequestDurationMillis(metricConfig),
 		otelchimetric.NewRequestInFlight(metricConfig),
 		otelchimetric.NewResponseSizeBytes(metricConfig),
+		middleware.Heartbeat("/ping"),
 	)
 
-	r.Use(middleware.Heartbeat("/ping"))
-
 	r.Post("/", app.Broker)
-
 	r.Post("/handle", app.HandleSubmission)
 
 	return r
